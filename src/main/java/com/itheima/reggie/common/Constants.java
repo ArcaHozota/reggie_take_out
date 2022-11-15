0b04b12f7b696612b09496bfbb2b4e4fc3ce1fd3
ArcaHozota <arkamahozota@gmail.com>
2022-11-15 20:36:28 +0900

    1.013

--- src/main/java/com/itheima/reggie/common/Constants.java
+++ src/main/java/com/itheima/reggie/common/Constants.java
@@ -6,40 +6,51 @@ package com.itheima.reggie.common;
  *
  * @author Administrator
  */
-public class Constants {
+public final class Constants {
 
     /**
      * 未登錄狀態
      */
-    public static final String NOT_LOGIN = "NOT_LOGIN";
+    public static String NOT_LOGIN = "NOT_LOGIN";
 
     /**
      * 登錄失敗
      */
-    public static final String LOGIN_FAILED = "LOGIN_FAILED";
+    public static String LOGIN_FAILED = "LOGIN_FAILED";
 
     /**
      * 項目等待超時
      */
-    public static final String TIME_OUT = "TIMEOUT";
+    public static String TIME_OUT = "TIMEOUT";
 
     /**
      * 賬號被禁用
      */
-    public static final String FORBIDDEN = "ACCOUNT_PROHIBITED";
+    public static String FORBIDDEN = "ACCOUNT_PROHIBITED";
 
     /**
      * 初始密碼
      */
-    public static final String PRIMARY_CODE = "111111";
+    public static String PRIMARY_CODE = "111111";
 
     /**
      * 出現異常
      */
-    public static final String ERROR = "ERROR_OCCURRED";
+    public static String ERROR = "ERROR_OCCURRED";
 
     /**
      * 重複的主鍵值
      */
-    public static final String DUPLICATED_KEY = "Duplicate entry";
+    public static String DUPLICATED_KEY = "Duplicate entry";
+
+    /**
+     * 獲取Entity類名
+     *
+     * @param obj
+     * @return class_name
+     */
+    public static String getEntityName(Object obj) {
+        return obj.getClass().getSimpleName().toString().toLowerCase();
+    }
+
 }
