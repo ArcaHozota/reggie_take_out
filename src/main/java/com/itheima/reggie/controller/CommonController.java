1694df184e96ad337e0b20525fc5cf44f7026941
ArcaHozota <arkamahozota@gmail.com>
2022-11-22 12:06:02 +0900

    1.41d

---
+++ src/main/java/com/itheima/reggie/controller/CommonController.java
@@ -0,0 +1,16 @@
+package com.itheima.reggie.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 通用模塊控制器
+ * 
+ * @author Administrator
+ * @date 2022-11-22
+ */
+@RestController
+@RequestMapping("/common")
+public class CommonController {
+
+}
