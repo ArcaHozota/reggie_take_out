c8cf29acb4e80d19beb75af6f3eca3b86f5632d3
ArcaHozota <arkamahozota@gmail.com>
2022-11-29 08:39:33 +0900

    2.13

---
+++ src/main/java/com/itheima/reggie/mapper/SetmealDishDao.java
@@ -0,0 +1,14 @@
+package com.itheima.reggie.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itheima.reggie.entity.SetmealDish;
+
+/**
+ * @author Administrator
+ * @date 2022-11-29
+ */
+@Mapper
+public interface SetmealDishDao extends BaseMapper<SetmealDish> {
+}
