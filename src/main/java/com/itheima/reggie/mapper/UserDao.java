00364ef5a8e780be643db48c70793be48f098140
ArcaHozota <arkamahozota@gmail.com>
2022-12-03 22:39:30 +0900

    2.59a

--- src/main/java/com/itheima/reggie/mapper/UserDao.java
+++ src/main/java/com/itheima/reggie/mapper/UserDao.java
@@ -1,5 +1,7 @@
 package com.itheima.reggie.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.itheima.reggie.entity.User;
 
@@ -7,5 +9,6 @@ import com.itheima.reggie.entity.User;
  * @author Administrator
  * @date 2022-11-29
  */
+@Mapper
 public interface UserDao extends BaseMapper<User> {
 }
