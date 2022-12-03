56e1fa8aeacdcdc9ead86e1645221982b0a61002
ArcaHozota <arkamahozota@gmail.com>
2022-12-04 03:16:17 +0900

    2.62

--- src/main/java/com/itheima/reggie/common/CustomMessage.java
+++ src/main/java/com/itheima/reggie/common/CustomMessage.java
@@ -8,64 +8,79 @@ package com.itheima.reggie.common;
  */
 public final class CustomMessage {
 
-    /**
-     * 分類信息添加成功
-     */
-    public static final String SRP001 = "分類信息添加成功";
+	/**
+	 * 分類信息添加成功
+	 */
+	public static final String SRP001 = "分類信息添加成功";
 
-    /**
-     * 分類信息修改成功
-     */
-    public static final String SRP002 = "分類信息修改成功";
+	/**
+	 * 分類信息修改成功
+	 */
+	public static final String SRP002 = "分類信息修改成功";
 
-    /**
-     * 分類信息刪除成功
-     */
-    public static final String SRP003 = "分類信息刪除成功";
+	/**
+	 * 分類信息刪除成功
+	 */
+	public static final String SRP003 = "分類信息刪除成功";
 
-    /**
-     * 菜品信息添加成功
-     */
-    public static final String SRP004 = "菜品信息添加成功";
+	/**
+	 * 菜品信息添加成功
+	 */
+	public static final String SRP004 = "菜品信息添加成功";
 
-    /**
-     * 菜品信息刪除成功
-     */
-    public static final String SRP005 = "菜品信息刪除成功";
+	/**
+	 * 菜品信息刪除成功
+	 */
+	public static final String SRP005 = "菜品信息刪除成功";
 
-    /**
-     * 員工信息添加成功
-     */
-    public static final String SRP006 = "員工信息添加成功";
+	/**
+	 * 員工信息添加成功
+	 */
+	public static final String SRP006 = "員工信息添加成功";
 
-    /**
-     * 成功退出登錄
-     */
-    public static final String SRP007 = "成功退出登錄";
+	/**
+	 * 成功退出登錄
+	 */
+	public static final String SRP007 = "成功退出登錄";
 
-    /**
-     * 員工信息修改成功
-     */
-    public static final String SRP008 = "員工信息修改成功";
+	/**
+	 * 員工信息修改成功
+	 */
+	public static final String SRP008 = "員工信息修改成功";
 
-    /**
-     * 分類刪除出錯
-     */
-    public static final String ERP090 = "當前分類下關聯了菜品或者套餐不能刪除";
+	/**
+	 * 分類刪除出錯
+	 */
+	public static final String ERP090 = "當前分類下關聯了菜品或者套餐不能刪除";
 
-    /**
-     * 套餐信息添加成功
-     */
-    public static final String SRP010 = "套餐信息添加成功";
+	/**
+	 * 套餐信息添加成功
+	 */
+	public static final String SRP010 = "套餐信息添加成功";
 
-    /**
-     * 套餐信息刪除成功
-     */
-    public static final String SRP011 = "套餐信息刪除成功";
+	/**
+	 * 套餐信息刪除成功
+	 */
+	public static final String SRP011 = "套餐信息刪除成功";
 
-    /**
-     * 套餐刪除出錯
-     */
-    public static final String ERP012 = "該套餐正在售賣中，無法刪除";
+	/**
+	 * 套餐刪除出錯
+	 */
+	public static final String ERP012 = "該套餐正在售賣中，無法刪除";
+
+	/**
+	 * 手機號碼不能為空
+	 */
+	public static final String ERP013 = "手機號碼不能為空";
+
+	/**
+	 * 驗證碼認證失敗
+	 */
+	public static final String ERP014 = "驗證碼認證失敗";
+
+	/**
+	 * 驗證訊息發送成功
+	 */
+	public static final String SRP015 = "驗證訊息發送成功";
 
 }
---
+++ src/main/java/com/itheima/reggie/controller/UserController.java
@@ -0,0 +1,90 @@
+package com.itheima.reggie.controller;
+
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.itheima.reggie.common.CustomMessage;
+import com.itheima.reggie.common.Reggie;
+import com.itheima.reggie.entity.User;
+import com.itheima.reggie.service.UserService;
+import com.itheima.reggie.utils.ComparisonUtils;
+import com.itheima.reggie.utils.SMSUtils;
+import com.itheima.reggie.utils.ValidateCodeUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@Resource
+	private UserService userService;
+
+	/**
+	 * 用戸登錄
+	 * 
+	 * @param user    用戸實體類
+	 * @param session 本次會話
+	 * @return R.success(登錄成功的信息)
+	 */
+	@PostMapping("/login")
+	public Reggie<User> login(@RequestBody final Map<String, String> userMap, final HttpSession session) {
+		// 獲取手機號；
+		final String phoneNo = userMap.get("phoneNo").toString();
+		// 獲取驗證碼；
+		final String code = userMap.get("code").toString();
+		// 獲取Session中保存的驗證碼；
+		final Object codeInSession = session.getAttribute(phoneNo);
+		// 進行驗證碼的比對；
+		if (ComparisonUtils.isNotEqual(null, codeInSession) && code.equals(codeInSession)) {
+			// 認證成功，放行登錄並驗證是否為新注冊手機號；
+			final LambdaQueryWrapper<User> queryWrapper = Wrappers.lambdaQuery(new User());
+			queryWrapper.eq(User::getPhoneNo, phoneNo);
+			User user = userService.getOne(queryWrapper);
+			// 如果是新用戸則自動完成注冊；
+			if (ComparisonUtils.isEqual(user, null)) {
+				user = new User();
+				user.setPhoneNo(phoneNo);
+				user.setStatus(1);
+				userService.save(user);
+			}
+			return Reggie.success(user);
+		}
+		return Reggie.error(CustomMessage.ERP014);
+	}
+
+	/**
+	 * 用戸登錄認證訊息發送
+	 * 
+	 * @param user    用戸實體類
+	 * @param session 本次會話
+	 * @return R.success(訊息發送成功的信息)
+	 */
+	@PostMapping("/sendMsg")
+	public Reggie<String> sendMsg(@RequestBody final User user, final HttpSession session) {
+		// 獲取手機號；
+		final String phoneNo = user.getPhoneNo();
+		if (StringUtils.isNotEmpty(phoneNo)) {
+			// 生成隨機的6位數驗證碼；
+			final String code = ValidateCodeUtils.generateValidateCode(6).toString();
+			// 將生成的驗證碼保存到Session中；
+			session.setAttribute(phoneNo, code);
+			log.info("本次的驗證碼為：{}", code);
+			// 調用阿里雲提供的訊息服務API完成送訊；
+			SMSUtils.sendMessage("瑞吉外賣", "00024", phoneNo, code);
+			return Reggie.success(CustomMessage.SRP015);
+		}
+		return Reggie.error(CustomMessage.ERP013);
+	}
+}
