80f6fc8e13ede843061dacf097f596a192bd821e
kyut <kyut@kyut-virtual-machine>
2023-01-07 23:45:03 +0900

    3.40

--- src/main/java/com/itheima/reggie/entity/Category.java
+++ src/main/java/com/itheima/reggie/entity/Category.java
@@ -3,84 +3,12 @@ package com.itheima.reggie.entity;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import org.hibernate.annotations.GenericGenerator;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import lombok.Getter;
-import lombok.Setter;
-
 /**
  * 分類管理實體類
  *
  * @author Administrator
  */
-@Getter
-@Setter
-@Entity
-@Table(name = "category")
-public class Category implements Serializable {
-
+public record Category(Long id, Integer type, String name, Integer sort, LocalDateTime createTime,
+		LocalDateTime updateTime, Long createUser, Long updateUser, Integer isDeleted) implements Serializable {
 	private static final long serialVersionUID = -5583580956537498025L;
-
-	/**
-	 * ID
-	 */
-	@Id
-	@GenericGenerator(name = "snowflakeId", strategy = "com.itheima.reggie.utils.SnowflakeIdGenerator")
-	@GeneratedValue(generator = "snowflakeId")
-	private Long id;
-
-	/**
-	 * 類型：1、菜品分類；2、套餐分類；
-	 */
-	private Integer type;
-
-	/**
-	 * 分類名稱
-	 */
-	private String name;
-
-	/**
-	 * 順序
-	 */
-	private Integer sort;
-
-	/**
-	 * 創建時間
-	 */
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	@Column(name = "create_time", updatable = false)
-	private LocalDateTime createTime;
-
-	/**
-	 * 創建人
-	 */
-	@Column(name = "create_user", updatable = false)
-	private Long createUser;
-
-	/**
-	 * 更新時間
-	 */
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	@Column(name = "update_time")
-	private LocalDateTime updateTime;
-
-	/**
-	 * 修改者
-	 */
-	@Column(name = "update_user")
-	private Long updateUser;
-
-	/**
-	 * 邏輯刪除字段
-	 */
-	@Column(name = "is_deleted")
-	private Integer isDeleted;
 }
--- src/main/java/com/itheima/reggie/entity/Dish.java
+++ src/main/java/com/itheima/reggie/entity/Dish.java
@@ -4,109 +4,13 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import org.hibernate.annotations.GenericGenerator;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import lombok.Getter;
-import lombok.Setter;
-
 /**
  * 菜品實體類
  *
  * @author Administrator
  */
-@Getter
-@Setter
-@Entity
-@Table(name = "dish")
-public class Dish implements Serializable {
-
+public record Dish(Long id, String name, Long categoryId, BigDecimal price, String code, String image,
+		String description, Integer status, Integer sort, LocalDateTime createTime, Long createUser,
+		LocalDateTime updateTime, Long updateUser, Integer isDeleted) implements Serializable {
 	private static final long serialVersionUID = 6089472680388107154L;
-
-	/**
-	 * ID
-	 */
-	@Id
-	@GenericGenerator(name = "snowflakeId", strategy = "com.itheima.reggie.utils.SnowflakeIdGenerator")
-	@GeneratedValue(generator = "snowflakeId")
-	private Long id;
-
-	/**
-	 * 菜品名稱
-	 */
-	private String name;
-
-	/**
-	 * 菜品分類ID
-	 */
-	private Long categoryId;
-
-	/**
-	 * 菜品價格
-	 */
-	private BigDecimal price;
-
-	/**
-	 * 商品碼
-	 */
-	private String code;
-
-	/**
-	 * 圖片
-	 */
-	private String image;
-
-	/**
-	 * 描述信息
-	 */
-	private String description;
-
-	/**
-	 * 菜品銷售狀態:0停售, 1在售;
-	 */
-	private Integer status;
-
-	/**
-	 * 順序
-	 */
-	private Integer sort;
-
-	/**
-	 * 創建時間
-	 */
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	@Column(name = "create_time", updatable = false)
-	private LocalDateTime createTime;
-
-	/**
-	 * 創建人
-	 */
-	@Column(name = "create_user", updatable = false)
-	private Long createUser;
-
-	/**
-	 * 更新時間
-	 */
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	@Column(name = "update_time")
-	private LocalDateTime updateTime;
-
-	/**
-	 * 修改者
-	 */
-	@Column(name = "update_user")
-	private Long updateUser;
-
-	/**
-	 * 邏輯刪除字段
-	 */
-	@Column(name = "is_deleted")
-	private Integer isDeleted;
 }
