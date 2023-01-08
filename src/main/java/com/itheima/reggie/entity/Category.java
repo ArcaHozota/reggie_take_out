98d27ee1a1243f61fdd010e11bae018b81df8232
kyut <kyut@kyut-virtual-machine>
2023-01-08 12:19:54 +0900

    3.41

--- src/main/java/com/itheima/reggie/entity/Category.java
+++ src/main/java/com/itheima/reggie/entity/Category.java
@@ -3,12 +3,21 @@ package com.itheima.reggie.entity;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+
 /**
  * 分類管理實體類
  *
  * @author Administrator
  */
-public record Category(Long id, Integer type, String name, Integer sort, LocalDateTime createTime,
-		LocalDateTime updateTime, Long createUser, Long updateUser, Integer isDeleted) implements Serializable {
+public record Category(@TableId Long id, Integer type, String name, Integer sort,
+		@TableField(fill = FieldFill.INSERT) LocalDateTime createTime,
+		@TableField(fill = FieldFill.INSERT_UPDATE) LocalDateTime updateTime,
+		@TableField(fill = FieldFill.INSERT) Long createUser,
+		@TableField(fill = FieldFill.INSERT_UPDATE) Long updateUser, @TableLogic Integer isDeleted)
+		implements Serializable {
 	private static final long serialVersionUID = -5583580956537498025L;
 }
--- src/main/java/com/itheima/reggie/entity/Dish.java
+++ src/main/java/com/itheima/reggie/entity/Dish.java
@@ -4,13 +4,21 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+
 /**
  * 菜品實體類
  *
  * @author Administrator
  */
-public record Dish(Long id, String name, Long categoryId, BigDecimal price, String code, String image,
-		String description, Integer status, Integer sort, LocalDateTime createTime, Long createUser,
-		LocalDateTime updateTime, Long updateUser, Integer isDeleted) implements Serializable {
+public record Dish(@TableId Long id, String name, Long categoryId, BigDecimal price, String code, String image,
+		String description, Integer status, Integer sort, @TableField(fill = FieldFill.INSERT) LocalDateTime createTime,
+		@TableField(fill = FieldFill.INSERT_UPDATE) LocalDateTime updateTime,
+		@TableField(fill = FieldFill.INSERT) Long createUser,
+		@TableField(fill = FieldFill.INSERT_UPDATE) Long updateUser, @TableLogic Integer isDeleted)
+		implements Serializable {
 	private static final long serialVersionUID = 6089472680388107154L;
 }
