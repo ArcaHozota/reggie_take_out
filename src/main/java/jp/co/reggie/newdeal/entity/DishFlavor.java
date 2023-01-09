bc1f802f8e5372713cb652e7e8541e8b2a64eadb
kyut <kyut@kyut-virtual-machine>
2023-01-09 20:51:30 +0900

    3.61

--- src/main/java/jp/co/reggie/newdeal/entity/DishFlavor.java
+++ src/main/java/jp/co/reggie/newdeal/entity/DishFlavor.java
@@ -8,67 +8,16 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 
-import lombok.Data;
-
 /**
  * 菜品口味實體類
  *
  * @author Administrator
  */
-@Data
-public class DishFlavor implements Serializable {
-
+public record DishFlavor(@TableId Long id, Long dishId, String name, String value,
+		@TableField(fill = FieldFill.INSERT) LocalDateTime createTime,
+		@TableField(fill = FieldFill.INSERT_UPDATE) LocalDateTime updateTime,
+		@TableField(fill = FieldFill.INSERT) Long createUser,
+		@TableField(fill = FieldFill.INSERT_UPDATE) Long updateUser, @TableLogic Integer isDeleted)
+		implements Serializable {
 	private static final long serialVersionUID = 6752106293794210881L;
-
-	/**
-	 * ID
-	 */
-	@TableId
-	private Long id;
-
-	/**
-	 * 菜品ID
-	 */
-	private Long dishId;
-
-	/**
-	 * 口味名稱
-	 */
-	private String name;
-
-	/**
-	 * 口味數據list
-	 */
-	private String value;
-
-	/**
-	 * 創建時間
-	 */
-	@TableField(fill = FieldFill.INSERT)
-	private LocalDateTime createTime;
-
-	/**
-	 * 更新時間
-	 */
-	@TableField(fill = FieldFill.INSERT_UPDATE)
-	private LocalDateTime updateTime;
-
-	/**
-	 * 創建人
-	 */
-	@TableField(fill = FieldFill.INSERT)
-	private Long createUser;
-
-	/**
-	 * 修改者
-	 */
-	@TableField(fill = FieldFill.INSERT_UPDATE)
-	private Long updateUser;
-
-	/**
-	 * 邏輯刪除字段
-	 */
-	@TableLogic
-	private Integer isDeleted;
-
 }
--- src/main/java/jp/co/reggie/newdeal/entity/Setmeal.java
+++ src/main/java/jp/co/reggie/newdeal/entity/Setmeal.java
@@ -6,87 +6,19 @@ import java.time.LocalDateTime;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
-
-import lombok.Data;
 
 /**
  * 套餐實體類
  *
  * @author Administrator
  */
-@Data
-public class Setmeal implements Serializable {
-
+public record Setmeal(@TableId Long id, Long categoryId, String name, BigDecimal price, Integer status, String code,
+		String description, String image, @TableField(fill = FieldFill.INSERT) LocalDateTime createTime,
+		@TableField(fill = FieldFill.INSERT_UPDATE) LocalDateTime updateTime,
+		@TableField(fill = FieldFill.INSERT) Long createUser,
+		@TableField(fill = FieldFill.INSERT_UPDATE) Long updateUser, @TableLogic Integer isDeleted)
+		implements Serializable {
 	private static final long serialVersionUID = 4020217756505140488L;
-
-	/**
-	 * ID
-	 */
-	private Long id;
-
-	/**
-	 * 分類ID
-	 */
-	private Long categoryId;
-
-	/**
-	 * 套餐名稱
-	 */
-	private String name;
-
-	/**
-	 * 套餐價格
-	 */
-	private BigDecimal price;
-
-	/**
-	 * 套餐在售狀態：0停售, 1在售;
-	 */
-	private Integer status;
-
-	/**
-	 * 編碼
-	 */
-	private String code;
-
-	/**
-	 * 描述信息
-	 */
-	private String description;
-
-	/**
-	 * 圖片
-	 */
-	private String image;
-
-	/**
-	 * 創建時間
-	 */
-	@TableField(fill = FieldFill.INSERT)
-	private LocalDateTime createTime;
-
-	/**
-	 * 更新時間
-	 */
-	@TableField(fill = FieldFill.INSERT_UPDATE)
-	private LocalDateTime updateTime;
-
-	/**
-	 * 創建人
-	 */
-	@TableField(fill = FieldFill.INSERT)
-	private Long createUser;
-
-	/**
-	 * 修改者
-	 */
-	@TableField(fill = FieldFill.INSERT_UPDATE)
-	private Long updateUser;
-
-	/**
-	 * 邏輯刪除字段
-	 */
-	@TableLogic
-	private Integer isDeleted;
 }
