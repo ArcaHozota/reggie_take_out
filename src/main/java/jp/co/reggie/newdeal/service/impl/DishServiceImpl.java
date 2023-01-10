9b4aa52eab73cd99d7a920114a96bd4ee4c52b4c
kyut <kyut@kyut-virtual-machine>
2023-01-10 15:57:37 +0900

    3.67

--- src/main/java/jp/co/reggie/newdeal/service/impl/DishServiceImpl.java
+++ src/main/java/jp/co/reggie/newdeal/service/impl/DishServiceImpl.java
@@ -3,12 +3,10 @@ package jp.co.reggie.newdeal.service.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 import jp.co.reggie.newdeal.dto.DishDto;
@@ -44,21 +42,24 @@ public class DishServiceImpl extends ServiceImpl<DishDao, Dish> implements DishS
 	@Transactional
 	public void saveWithFlavour(final DishDto dishDto) {
 		// 保存菜品的基本信息到菜品表；
-		this.save(dishDto);
+		final Dish dish = new Dish(dishDto.id(), dishDto.name(), dishDto.categoryId(), dishDto.price(), dishDto.code(),
+				dishDto.image(), dishDto.description(), dishDto.status(), dishDto.sort(), null, null, null, null, 0);
+		this.save(dish);
 		// 獲取菜品口味的集合；
-		List<DishFlavor> flavors = dishDto.getFlavors();
+		List<DishFlavor> flavors = dishDto.flavors();
 		// 將菜品ID設置到口味集合中；
 		flavors = flavors.stream().map((item) -> {
-			item.setDishId(dishDto.getId());
-			return item;
+			final DishFlavor dishFlavor = new DishFlavor(item.id(), dishDto.id(), item.name(), item.value(),
+					item.createTime(), item.updateTime(), item.createUser(), item.updateUser(), item.isDeleted());
+			return dishFlavor;
 		}).collect(Collectors.toList());
 		// 保存 菜品的口味數據到口味表；
-		dishFlavorService.saveBatch(flavors);
+		this.dishFlavorService.saveBatch(flavors);
 	}
 
 	/**
 	 * 根據ID查詢菜品信息以及對應的口味信息
-	 * 
+	 *
 	 * @param id 菜品ID
 	 * @return dishDto 菜品及口味數據傳輸類
 	 */
@@ -66,16 +67,15 @@ public class DishServiceImpl extends ServiceImpl<DishDao, Dish> implements DishS
 	public DishDto getByIdWithFlavour(final Long id) {
 		// 查詢菜品的基本信息；
 		final Dish dish = this.getById(id);
-		// 聲明一個菜品及口味數據傳輸類對象；
-		final DishDto dishDto = new DishDto();
-		// 拷貝屬性；
-		BeanUtils.copyProperties(dish, dishDto);
 		// 查詢當前菜品所對應的口味信息；
-		final LambdaQueryWrapper<DishFlavor> queryWrapper = Wrappers.lambdaQuery(new DishFlavor());
-		queryWrapper.eq(DishFlavor::getId, dish.getId());
+		final LambdaQueryWrapper<DishFlavor> queryWrapper = new LambdaQueryWrapper<>();
+		queryWrapper.eq(DishFlavor::id, dish.id());
 		// 獲取菜品口味列表；
-		final List<DishFlavor> flavors = dishFlavorService.list(queryWrapper);
-		dishDto.setFlavors(flavors);
+		final List<DishFlavor> flavors = this.dishFlavorService.list(queryWrapper);
+		// 聲明一個菜品及口味數據傳輸類對象並拷貝屬性；
+		final DishDto dishDto = new DishDto(dish.id(), dish.name(), dish.categoryId(), dish.price(), dish.code(),
+				dish.image(), dish.description(), dish.status(), dish.sort(), dish.createTime(), dish.updateTime(),
+				dish.createUser(), dish.updateUser(), dish.isDeleted(), flavors, null, null);
 		return dishDto;
 	}
 
@@ -87,19 +87,23 @@ public class DishServiceImpl extends ServiceImpl<DishDao, Dish> implements DishS
 	@Override
 	@Transactional
 	public void updateWithFlavour(final DishDto dishDto) {
+		// 聲明一個菜品實體類對象；
+		final Dish dish = new Dish(dishDto.id(), dishDto.name(), dishDto.categoryId(), dishDto.price(), dishDto.code(),
+				dishDto.image(), dishDto.description(), dishDto.status(), dishDto.sort(), null, null, null, null, 0);
 		// 更新菜品信息；
-		this.updateById(dishDto);
+		this.updateById(dish);
 		// 清理當前菜品所對應的口味信息；
-		final LambdaQueryWrapper<DishFlavor> queryWrapper = Wrappers.lambdaQuery(new DishFlavor());
-		queryWrapper.eq(DishFlavor::getId, dishDto.getId());
-		dishFlavorService.remove(queryWrapper);
+		final LambdaQueryWrapper<DishFlavor> queryWrapper = new LambdaQueryWrapper<>();
+		queryWrapper.eq(DishFlavor::id, dishDto.id());
+		this.dishFlavorService.remove(queryWrapper);
 		// 添加當前菜品的口味數據；
-		List<DishFlavor> flavors = dishDto.getFlavors();
+		List<DishFlavor> flavors = dishDto.flavors();
 		// 將菜品ID設置到口味集合中；
 		flavors = flavors.stream().map((item) -> {
-			item.setDishId(dishDto.getId());
-			return item;
+			final DishFlavor dishFlavor = new DishFlavor(item.id(), dishDto.id(), item.name(), item.value(),
+					item.createTime(), item.updateTime(), item.createUser(), item.updateUser(), item.isDeleted());
+			return dishFlavor;
 		}).collect(Collectors.toList());
-		dishFlavorService.saveBatch(flavors);
+		this.dishFlavorService.saveBatch(flavors);
 	}
 }
--- src/main/java/jp/co/reggie/newdeal/service/impl/SetmealServiceImpl.java
+++ src/main/java/jp/co/reggie/newdeal/service/impl/SetmealServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 import jp.co.reggie.newdeal.common.CustomException;
@@ -44,17 +43,22 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealDao, Setmeal> impleme
 	@Override
 	@Transactional
 	public void saveWithDish(final SetmealDto setmealDto) {
+		final Setmeal setmeal = new Setmeal(setmealDto.id(), setmealDto.categoryId(), setmealDto.name(),
+				setmealDto.price(), setmealDto.status(), setmealDto.code(), setmealDto.description(),
+				setmealDto.image(), null, null, null, null, 0);
 		// 保存套餐的基本信息；
-		this.save(setmealDto);
+		this.save(setmeal);
 		// 獲取套餐菜品關聯集合；
-		List<SetmealDish> setmealDishes = setmealDto.getSetmealDishes();
+		List<SetmealDish> setmealDishes = setmealDto.setmealDishes();
 		// 獲取菜品ID並插入集合；
 		setmealDishes = setmealDishes.stream().map((item) -> {
-			item.setDishId(setmealDto.getId());
-			return item;
+			final SetmealDish setmealDish = new SetmealDish(item.id(), item.setmealId(), setmealDto.id(), item.name(),
+					item.price(), item.copies(), item.sort(), item.createTime(), item.updateTime(), item.createUser(),
+					item.updateUser(), item.isDeleted());
+			return setmealDish;
 		}).collect(Collectors.toList());
 		// 保存套餐和菜品的關聯關係；
-		setmealDishService.saveBatch(setmealDishes);
+		this.setmealDishService.saveBatch(setmealDishes);
 	}
 
 	/**
@@ -65,9 +69,9 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealDao, Setmeal> impleme
 	@Override
 	public void removeWithDish(final List<Long> ids) {
 		// 查詢套餐狀態以確認是否可以刪除；
-		final LambdaQueryWrapper<Setmeal> queryWrapper = Wrappers.lambdaQuery(new Setmeal());
-		queryWrapper.in(Setmeal::getId, ids);
-		queryWrapper.eq(Setmeal::getStatus, 1);
+		final LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+		queryWrapper.in(Setmeal::id, ids);
+		queryWrapper.eq(Setmeal::status, 1);
 		final long count = this.count(queryWrapper);
 		if (count > 0) {
 			// 如果無法刪除，則抛出異常；
@@ -76,8 +80,8 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealDao, Setmeal> impleme
 		// 刪除套餐表中的數據；
 		this.removeByIds(ids);
 		// 刪除套餐口味表中的數據；
-		final LambdaQueryWrapper<SetmealDish> lambdaQueryWrapper = Wrappers.lambdaQuery(new SetmealDish());
-		lambdaQueryWrapper.in(SetmealDish::getSetmealId, ids);
-		setmealDishService.remove(lambdaQueryWrapper);
+		final LambdaQueryWrapper<SetmealDish> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+		lambdaQueryWrapper.in(SetmealDish::setmealId, ids);
+		this.setmealDishService.remove(lambdaQueryWrapper);
 	}
 }
