50238f2a2fec2e95764b33477b4cbbed772a8f70
ArcaHozota <arkamahozota@gmail.com>
2023-01-12 23:24:45 +0900

    4.14

--- src/main/java/jp/co/reggie/newdeal/dto/DishDto.java
+++ src/main/java/jp/co/reggie/newdeal/dto/DishDto.java
@@ -1,22 +1,36 @@
 package jp.co.reggie.newdeal.dto;
 
-import java.io.Serial;
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import jp.co.reggie.newdeal.entity.DishFlavor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * 菜品以及口味數據傳輸專用類
  *
  * @author Administrator
  */
-public record DishDto(Long id, String name, Long categoryId, BigDecimal price, String code, String image,
-		String description, Integer status, Integer sort, LocalDateTime createTime, LocalDateTime updateTime,
-		Long createUser, Long updateUser, Integer isDeleted, List<DishFlavor> flavors, String categoryName,
-		Integer copy) implements Serializable {
-	@Serial
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class DishDto implements Serializable {
+
 	private static final long serialVersionUID = 363782191754121480L;
+
+	/**
+	 * 口味集合
+	 */
+	private final List<DishFlavor> flavors = new ArrayList<>();
+
+	/**
+	 * 分類名稱
+	 */
+	private String categoryName;
+
+	/**
+	 * 複製品
+	 */
+	private Integer copy;
 }
--- src/main/java/jp/co/reggie/newdeal/dto/SetmealDto.java
+++ src/main/java/jp/co/reggie/newdeal/dto/SetmealDto.java
@@ -1,22 +1,30 @@
 package jp.co.reggie.newdeal.dto;
 
-import java.io.Serial;
-import java.io.Serializable;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.List;
 
+import jp.co.reggie.newdeal.entity.Setmeal;
 import jp.co.reggie.newdeal.entity.SetmealDish;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * 套餐數據傳輸專用類
  *
  * @author Administrator
  */
-public record SetmealDto(Long id, Long categoryId, String name, BigDecimal price, Integer status, String code,
-		String description, String image, LocalDateTime createTime, LocalDateTime updateTime, Long createUser,
-		Long updateUser, Integer isDeleted, List<SetmealDish> setmealDishes, String categoryName)
-		implements Serializable {
-	@Serial
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class SetmealDto extends Setmeal {
+
 	private static final long serialVersionUID = 5174917893420797875L;
+
+	/**
+	 * 套餐集合
+	 */
+	private List<SetmealDish> setmealDishes;
+
+	/**
+	 * 分類名稱
+	 */
+	private String categoryName;
 }
