341bbd2f6f8201e06f39fea21a868afec9a3e5a8
ArcaHozota <arkamahozota@gmail.com>
2023-01-12 21:19:54 +0900

    3.87

--- src/main/java/jp/co/reggie/newdeal/mapper/EmployeeMapper.java
+++ src/main/java/jp/co/reggie/newdeal/mapper/EmployeeMapper.java
@@ -5,10 +5,12 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import jp.co.reggie.newdeal.entity.Employee;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * @author Administrator
  */
 @Mapper
 public interface EmployeeMapper extends BaseMapper<Employee> {
+    Employee selectByUsername(@Param("username") final String userName);
 }
