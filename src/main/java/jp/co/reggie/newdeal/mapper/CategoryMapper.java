18d3191199fb5d29c9983c341f27eff2aca0fae6
ArcaHozota <arkamahozota@gmail.com>
2023-01-13 19:37:55 +0900

    4.23

--- src/main/java/jp/co/reggie/newdeal/mapper/CategoryMapper.java
+++ src/main/java/jp/co/reggie/newdeal/mapper/CategoryMapper.java
@@ -1,10 +1,13 @@
 package jp.co.reggie.newdeal.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 import jp.co.reggie.newdeal.entity.Category;
-
-import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author Administrator
@@ -12,4 +15,12 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface CategoryMapper extends BaseMapper<Category> {
+
+	/**
+	 * 根據類型查詢數據
+	 *
+	 * @param type 類型
+	 * @return List<Category>
+	 */
+	List<Category> selectByType(@Param("type") Integer categoryType);
 }
